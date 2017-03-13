440393affa39d1d8d75391cc5dafd50f428d11d6
Ben Mercer <ben@henroben.co.uk>
2017-03-13 15:37:35 +0000

    header and routes added

---
+++ src/components/Resources.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="jumbotron">
+           <h1>Super Secret Special Recipie</h1>
+            <p>test stuff</p>
+        </div>
+    );
+};
---
+++ src/components/header.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Header extends Component {
+    authButton() {
+        return <button className="btn btn-primary">Sign In</button>
+    }
+
+    render() {
+        return(
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <div className="container">
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/resources">Resources</Link>
+                        </li>
+                        <li className="nav-item">
+                            { this.authButton() }
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
